f2bdeb65c0f7f9cd5301ab11578843671698ce96
Marcus Lim <lim_marcus_97@hotmail.com>
2021-05-11 18:24:24 +0800

    Refactor

--- src/main/java/crm/Controller.java
+++ src/main/java/crm/Controller.java
@@ -1,6 +1,5 @@
 package crm;
 
-import crm.customer.Customer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
