3c934e8ee50099fc27ac08e8b5e4020b9e16d8c9
Marcus Lim <lim_marcus_97@hotmail.com>
2021-05-11 18:14:45 +0800

    Update Customer to entity model

--- src/main/java/crm/customer/Customer.java
+++ src/main/java/crm/customer/Customer.java
@@ -1,29 +1,65 @@
 package crm.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import static crm.util.Util.requireAllNonNull;
 
-public class Customer {
-  private final String firstName;
-  private final String lastName;
-  private final Email email;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
-  public Customer(String firstName, String lastName, Email email) {
+@Entity
+@Table(name = "customer")
+public class Customer {
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+  @Column(name = "first_name")
+  private String firstName;
+  @Column(name = "last_name")
+  private String lastName;
+  @Column(name = "email")
+  private String email;
+
+  public Customer(String firstName, String lastName, String email) {
     requireAllNonNull(firstName, lastName, email);
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
   }
 
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
   public String getFirstName() {
     return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
   }
 
   public String getLastName() {
     return lastName;
   }
 
-  public Email getEmail() {
+  public String getEmail() {
     return email;
+  }
+
+  public void setEmail(String value) {
+    email = value;
   }
 
   @Override
